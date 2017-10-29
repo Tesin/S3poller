9269143a7a62ef02f119bb314079cbc62d01b20a
Sean Brooks <sean@mhyway.com>
2017-10-28 21:17:47 -0500

    Save point 2017-10-28:21:18

--- config/config.exs
+++ config/config.exs
@@ -7,7 +7,8 @@ use Mix.Config
 # file won't be loaded nor affect the parent project. For this reason,
 # if you want to provide default values for your application for
 # 3rd-party users, it should be done in your "mix.exs" file.
-
+config :ex_aws,
+  region: "us-east-1"
 # You can configure your application as:
 #
 #     config :s3poller, key: :value
--- lib/s3poller.ex
+++ lib/s3poller.ex
@@ -2,6 +2,24 @@ defmodule S3poller do
 
   alias ExAws.S3
 
+  def start_agents do
+    Agent.start_link( fn -> build_initial_list() end, name: Current_state )
+    Agent.start_link( fn -> [[]] end, name: S3_placeholder )
+  end
+
+
+  def build_initial_list() do
+    response = retrieve_objects()
+
+    initial_list = Enum.map( response, &extract_item/1 )
+    initial_list
+  end
+
+  def extract_item( item ) do
+    %{ key: key, last_modified: last_modified } = item
+    [ key: key, last_modified: last_modified ]
+  end
+
 
   def retrieve_objects() do
     { :ok, %{ body: %{ contents: contents } } } =
@@ -12,23 +30,40 @@ defmodule S3poller do
   end
 
 
-  def build_initial_list() do
-    response = retrieve_objects()
+  def check_for_changes do
+    current_s3_state = retrieve_objects()
+    current_s3_keys  = Enum.map( current_s3_state, &extract_item/1 )
 
-    initialList = Enum.map( response, fn( %{ key: key, last_modified: last_modified } ) ->
-      [key: key, last_modified: last_modified]
+    current_agent_keys = Agent.get( Current_state, &(&1))
+
+    s3_map    = MapSet.new( current_s3_keys )
+    agent_map = MapSet.new( current_agent_keys )
+
+    Agent.update( S3_placeholder, fn() ->
+      current_s3_keys
     end )
 
-    initialList
+    Agent.update( Current_state, fn() ->
+      current_s3_keys
+    end )
+
+    MapSet.difference( s3_map, agent_map )
   end
 
 
-  def poll_for_changes do
-    response = retrieve_objects()
+  def poll do
+    diff = check_for_changes()
 
-    response
+    IO.inspect diff
+    :timer.sleep( 10000 )
+
+    poll()
   end
 
 
+  def run do
+    start_agents()
 
+    poll()
+  end
 end
